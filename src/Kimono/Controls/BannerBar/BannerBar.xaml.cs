a19c4e422c1959bdd80966e62b34358287348750
Alexander Mitchell-Robinson <amrykid@gmail.com>
2016-02-20 18:24:55 -0600

    Banner bar like Readit.

---
+++ src/Kimono/Controls/BannerBar/BannerBar.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Kimono.Controls
+{
+    public sealed partial class BannerBar : UserControl
+    {
+        public BannerBar()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty BannerTextProperty = DependencyProperty.Register("BannerText", typeof(string), typeof(BannerBar), new PropertyMetadata(""));
+        public string BannerText
+        {
+            get { return (string)GetValue(BannerTextProperty); }
+            set { SetValue(BannerTextProperty, value); }
+        }
+    }
+}
