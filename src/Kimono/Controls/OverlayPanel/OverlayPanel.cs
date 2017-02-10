611ec40e019bf133f166ac7d7247f58169d4026b
Alexander Mitchell-Robinson <amrykid@gmail.com>
2017-02-10 02:00:48 -0600

    Added overlay stuff based on the idea of: https://github.com/vlonjatg/progress-activity Will expand.

---
+++ src/Kimono/Controls/OverlayPanel/OverlayPanel.cs
@@ -0,0 +1,55 @@
+﻿using Kimono.Controls.SnackBar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace Kimono.Controls
+{
+    public sealed class OverlayPanel : ContentControl
+    {
+        private SnackBarManager snackManager = null;
+        private OverlayPanelManager overlayManager = null;
+        private Grid PART_SnackBarGrid = null;
+        private ContentPresenter PART_ContentPresenter = null;
+        private Grid PART_OverlayGrid = null;
+
+        public OverlayPanelManager OverlayProvider { get { return overlayManager; } }
+        public SnackBarManager SnackBarProvider { get { return snackManager; } }
+
+        public OverlayPanel()
+        {
+            this.DefaultStyleKey = typeof(OverlayPanel);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            PART_SnackBarGrid = GetTemplateChild(nameof(PART_SnackBarGrid)) as Grid;
+            PART_OverlayGrid = GetTemplateChild(nameof(PART_OverlayGrid)) as Grid;
+            PART_ContentPresenter = GetTemplateChild(nameof(PART_ContentPresenter)) as ContentPresenter;
+
+            snackManager = new SnackBarManager(PART_SnackBarGrid);
+            overlayManager = new OverlayPanelManager(this, PART_OverlayGrid);
+
+            base.OnApplyTemplate();
+        }
+
+        internal void HideContent()
+        {
+            PART_ContentPresenter.Visibility = Visibility.Collapsed;
+        }
+
+        internal void ShowContent()
+        {
+            PART_ContentPresenter.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ src/Kimono/Controls/OverlayPanel/OverlayPanelManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Kimono.Controls
+{
+    public sealed class OverlayPanelManager
+    {
+        private Grid overlayAreaGrid = null;
+        private IOverlayPanelScreen currentScreen = null;
+
+        internal OverlayPanel ParentPanel { get; private set; }
+
+        internal OverlayPanelManager(OverlayPanel parentPanel, Grid overlayArea)
+        {
+            ParentPanel = parentPanel;
+            overlayAreaGrid = overlayArea;
+        }
+
+        private void VerifyUIThread()
+        {
+            if (!Window.Current.Dispatcher.HasThreadAccess)
+                throw new InvalidOperationException("This method must be called on the UI thread.");
+        }
+
+        public void ShowScreen(IOverlayPanelScreen screen)
+        {
+            VerifyUIThread();
+
+            if (screen == null) throw new ArgumentNullException(nameof(screen));
+
+            if (!(screen is FrameworkElement))
+                throw new ArgumentException(nameof(screen) + " must inherit either directly or indirectly from FrameworkElement.");
+
+            if (currentScreen != null)
+            {
+                HideCurrentScreen();
+            }
+
+            ParentPanel.HideContent();
+
+            currentScreen = screen;
+
+            overlayAreaGrid.Children.Add(currentScreen as FrameworkElement);
+
+            screen.OnShown(ParentPanel);
+        }
+
+        public void HideCurrentScreen()
+        {
+            VerifyUIThread();
+
+            if (currentScreen != null)
+            {
+                currentScreen.OnClosed();
+
+                overlayAreaGrid.Children.Remove(currentScreen as FrameworkElement);
+
+                currentScreen = null;
+            }
+
+            ParentPanel.ShowContent();
+        }
+
+        public void ShowProgressOverlay()
+        {
+            ShowScreen(new ProgressOverlayScreen());
+        }
+    }
+}
